6c01bda98771a5eda0f8d03aa980b92e291a47d8
sanjita811 <79326586+sanjita811@users.noreply.github.com>
2023-02-16 12:25:17 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, PES1UG20CS380 have successfully built and run\n";
+  cout << "I, Sanjita Chandan Ballapur have successfully built and run\n";
   return 0;
 }
